9761b379aae8973e490ffb42cca10dd208d71e4f
Yannick Le Bras <yannicklebras@pop-os.localdomain>
2021-11-01 07:35:45 +0100

    bug sur les fonction renvoyant des valeurs non-entières affichage ! si la fonction est déjà utilisée.

--- voirQuestions.php
+++ voirQuestions.php
@@ -12,6 +12,8 @@ if ($questions->num_rows>0) {
 	echo("<table class=tableau>");
 	while ($question=$questions->fetch_assoc()) {
 		$idQuestion = $question["id"];
+		$requeteDejaTraitee = "SELECT * FROM REPONSES JOIN FILSROUGES ON REPONSES.filrouge = FILSROUGES.id WHERE FILSROUGES.question=$idQuestion";
+		$nbDT = $mysqli->query($requeteDejaTraitee)->num_rows;
 		$niveau = $question["niveau"];
 		echo("<tr><td width=200px>".$question["titre"]."</td>
 			  <td>".str_repeat("<span class='material-icons'>star</span>",$niveau).str_repeat("<span class='material-icons'>star_border</span>",5-$niveau)."</td>
@@ -19,7 +21,7 @@ if ($questions->num_rows>0) {
 			  <form action=actionQuestion.php style='display:inline' method=post>
 				<input type=hidden value='editer' name='editer'>
 				<input type=hidden value=$idQuestion name='idQuestion'>
-				<button type=submit class='bouton'><span class='material-icons'>edit</span></button>
+				<button type=submit class='bouton'><div><span class='material-icons' style=".(($nbDT>0)?" 'width:5px'":"'margin-right:5px'").">edit</span>".(($nbDT>0)?"<sub><span class='material-icons' style='scale:0.7;position:relative;color:red' title='Attention la question a déjà été traitée par un étudiant'>priority_high</span></sub>":"")."</div></button>
 			  </form>
 			  <form action=actionQuestion.php style='display:inline' method=post>
 				<input type=hidden value='voir' name='voir'>
